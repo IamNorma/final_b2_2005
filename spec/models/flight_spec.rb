892779cb9153b824dca2882d5c2efb3c77266bb0
Norma Lopez <norma_l304@yahoo.com>
2020-08-03 11:16:29 -0600

    Test #minors method

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -6,4 +6,25 @@ RSpec.describe Flight, type: :model do
     it {should have_many :flight_passengers}
     it {should have_many(:passengers).through(:flight_passengers)}
   end
+
+  describe "instance methods" do
+    it '#minors' do
+      delta = Airline.create!(name: "Delta Airlines")
+      flight1 = delta.flights.create!(number: "5689", date: "12/15/2019", time: "3:30pm MT", departure_city: "Denver", arrival_city: "Reno")
+
+      jackie = Passenger.create!(name: "Jackie Casas", age: 25)
+      jeanette = Passenger.create!(name: "Jeanette Medina", age: 65)
+      lupita = Passenger.create!(name: "Lupita Lopez", age: 22)
+      junior = Passenger.create!(name: "Junior Lopez", age: 14)
+      erick = Passenger.create!(name: "Erick Lopez", age: 6)
+
+      FlightPassenger.create!(flight: flight1, passenger: jackie)
+      FlightPassenger.create!(flight: flight1, passenger: jeanette)
+      FlightPassenger.create!(flight: flight1, passenger: lupita)
+      FlightPassenger.create!(flight: flight1, passenger: junior)
+      FlightPassenger.create!(flight: flight1, passenger: erick)
+
+      expect(flight1.minors).to eq(2)
+    end
+  end
 end
