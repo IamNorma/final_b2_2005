fce502f171bf64d61e75898e3cc2a5fd4e3530d4
Norma Lopez <norma_l304@yahoo.com>
2020-08-03 10:29:42 -0600

    Test displays form to add an existing flight to passenger

--- spec/features/passengers/show_spec.rb
+++ spec/features/passengers/show_spec.rb
@@ -59,4 +59,29 @@ RSpec.describe 'Passenger Show Page' do
 
     expect(current_path).to eq("/flights/#{flight1.id}")
   end
+
+  it 'displays form to add an existing flight' do
+    delta = Airline.create!(name: "Delta Airlines")
+    flight1 = delta.flights.create!(number: "5689", date: "12/15/2019", time: "3:30pm MT", departure_city: "Denver", arrival_city: "Reno")
+    flight2 = delta.flights.create!(number: "2356", date: "12/15/2019", time: "4:30pm PST", departure_city: "Reno", arrival_city: "Los Angeles")
+    flight3 = delta.flights.create!(number: "4512", date: "01/08/2020", time: "10:45am PST", departure_city: "Los Angeles", arrival_city: "Denver")
+    flight4 = delta.flights.create!(number: "7845", date: "02/02/2020", time: "12:45pm PST", departure_city: "Los Angeles", arrival_city: "Portland")
+
+    lupita = Passenger.create!(name: "Lupita Lopez", age: 22)
+
+    FlightPassenger.create!(flight: flight1, passenger: lupita)
+    FlightPassenger.create!(flight: flight2, passenger: lupita)
+    FlightPassenger.create!(flight: flight3, passenger: lupita)
+
+    visit "/passengers/#{lupita.id}"
+
+    expect(page).to_not have_content(flight4.number)
+
+    fill_in :number, with: "#{flight4.number}"
+
+    click_on "Submit"
+
+    expect(current_path).to eq("/passengers/#{lupita.id}")
+    expect(page).to have_link(flight4.number)
+  end
 end
