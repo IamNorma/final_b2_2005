19d7f55ab7d26f08b25430059b3a412296197fad
Norma Lopez <norma_l304@yahoo.com>
2020-08-03 09:51:36 -0600

    Test airline name is displayed

--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe 'Flight Show Page' do
     expect(page).to have_content(flight1.departure_city)
     expect(page).to have_content(flight1.arrival_city)
   end
+
+  it 'displays airline name' do
+    delta = Airline.create!(name: "Delta Airlines")
+    flight1 = delta.flights.create!(number: "5689", date: "12/15/2019", time: "3:30pm MT", departure_city: "Denver", arrival_city: "Reno")
+
+    visit "/flights/#{flight1.id}"
+
+    expect(page).to have_content(delta.name)
+  end
 end
