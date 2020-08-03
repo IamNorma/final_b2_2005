bd735b381b038978a7cc4c14c55aa67b73e386fe
Norma Lopez <norma_l304@yahoo.com>
2020-08-03 10:00:15 -0600

    Test passengers are displayed on flight show page

--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -22,4 +22,26 @@ RSpec.describe 'Flight Show Page' do
 
     expect(page).to have_content(delta.name)
   end
+
+  it 'displays name of all passengers on flight' do
+    delta = Airline.create!(name: "Delta Airlines")
+    flight1 = delta.flights.create!(number: "5689", date: "12/15/2019", time: "3:30pm MT", departure_city: "Denver", arrival_city: "Reno")
+
+    jackie = Passenger.create!(name: "Jackie Casas", age: 25)
+    lupita = Passenger.create!(name: "Lupita Lopez", age: 22)
+    junior = Passenger.create!(name: "Junior Lopez", age: 14)
+    erick = Passenger.create!(name: "Erick Lopez", age: 6)
+
+    FlightPassenger.create!(flight: flight1, passenger: jackie)
+    FlightPassenger.create!(flight: flight1, passenger: lupita)
+    FlightPassenger.create!(flight: flight1, passenger: junior)
+    FlightPassenger.create!(flight: flight1, passenger: erick)
+
+    visit "/flights/#{flight1.id}"
+
+    expect(page).to have_content(jackie.name)
+    expect(page).to have_content(lupita.name)
+    expect(page).to have_content(junior.name)
+    expect(page).to have_content(erick.name)
+  end
 end
