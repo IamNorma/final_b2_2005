e712227056b7675845be496eb7f6decb5ccf8127
Norma Lopez <norma_l304@yahoo.com>
2020-08-03 11:17:06 -0600

    Add #minors

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,8 @@ class Flight < ApplicationRecord
   belongs_to :airline
   has_many :flight_passengers
   has_many :passengers, through: :flight_passengers
+
+  def minors
+    passengers.where("age < ?", 18).count
+  end
 end
