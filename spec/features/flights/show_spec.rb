b67e6b8f78ad2f8c4ae4679d50c53c08f3389833
Norma Lopez <norma_l304@yahoo.com>
2020-08-03 11:15:18 -0600

    Update expected output text

--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -63,7 +63,7 @@ RSpec.describe 'Flight Show Page' do
 
     visit "/flights/#{flight1.id}"
 
-    expect(page).to have_content("Minors on flight: #{flight1.minors}")
-    expect(page).to have_content("Adults on flight: #{flight1.adults}")
+    expect(page).to have_content("Minors On This Flight: #{flight1.minors}")
+    expect(page).to have_content("Adults On This Flight: #{flight1.adults}")
   end
 end
