8840538ebe583c3efbcd099b4defeff6cb355510
Norma Lopez <norma_l304@yahoo.com>
2020-08-03 11:55:49 -0600

    Remove unnecessary brackets

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -1,7 +1,7 @@
 class FlightPassengersController < ApplicationController
   def create
     flight = Flight.where(number: params[:number]).last
-    FlightPassenger.create!({passenger_id: params[:id], flight_id: flight.id})
+    FlightPassenger.create!(passenger_id: params[:id], flight_id: flight.id)
     redirect_to "/passengers/#{params[:id]}"
   end
 end
