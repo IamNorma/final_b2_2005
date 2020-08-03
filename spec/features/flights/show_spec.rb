79b1aa41ddc2eef79004efae7776914f0bd67a9f
Norma Lopez <norma_l304@yahoo.com>
2020-08-03 11:05:48 -0600

    Test displays number of minors and number of adults

--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -44,4 +44,26 @@ RSpec.describe 'Flight Show Page' do
     expect(page).to have_content(junior.name)
     expect(page).to have_content(erick.name)
   end
+
+  it 'displays number of minors and adults on flight' do
+    delta = Airline.create!(name: "Delta Airlines")
+    flight1 = delta.flights.create!(number: "5689", date: "12/15/2019", time: "3:30pm MT", departure_city: "Denver", arrival_city: "Reno")
+
+    jackie = Passenger.create!(name: "Jackie Casas", age: 25)
+    jeanette = Passenger.create!(name: "Jeanette Medina", age: 65)
+    lupita = Passenger.create!(name: "Lupita Lopez", age: 22)
+    junior = Passenger.create!(name: "Junior Lopez", age: 14)
+    erick = Passenger.create!(name: "Erick Lopez", age: 6)
+
+    FlightPassenger.create!(flight: flight1, passenger: jackie)
+    FlightPassenger.create!(flight: flight1, passenger: jeanette)
+    FlightPassenger.create!(flight: flight1, passenger: lupita)
+    FlightPassenger.create!(flight: flight1, passenger: junior)
+    FlightPassenger.create!(flight: flight1, passenger: erick)
+
+    visit "/flights/#{flight1.id}"
+
+    expect(page).to have_content("Minors on flight: #{flight1.minors}")
+    expect(page).to have_content("Adults on flight: #{flight1.adults}")
+  end
 end
